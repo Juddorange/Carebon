4f5d0930ba58014f95eb1b7439519c4277692114
Maxime <justine.gander@gmail.com>
2019-09-25 18:47:50 +0200

    profile

--- server/routes/profile.js
+++ server/routes/profile.js
@@ -3,5 +3,11 @@ const router = express.Router();
 const User = require('../models/User');
 
 router.get('/profile', (req, res) => {
-	res.json(res);
+	User.findOne({ email: req.user.email })
+		.then((dbRes) => {
+			res.json(dbRes);
+		})
+		.catch((err) => console.log(err));
 });
+
+module.exports = router;
