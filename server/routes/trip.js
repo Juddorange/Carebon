1e32b65a9acaa609c9dfdb38bc6025e9a8349eb0
Loup A <loup.aparicio@gmail.com>
2019-09-26 12:23:50 +0200

    apis connected

--- server/routes/trip.js
+++ server/routes/trip.js
@@ -4,12 +4,14 @@ const axios = require('axios')
 const router = express.Router()
 
 function getCarbonPrint(distance, mode) {
-  return axios
-    .get(
-      `https://api.triptocarbon.xyz/v1/footprint?activity=${distance}&activityType=miles&country=def&mode=${mode}`
-    )
-    .then(res => res.data)
-    .catch(err => console.log(err))
+  return axios.get(
+    `https://api.triptocarbon.xyz/v1/footprint?activity=${distance}&activityType=miles&country=def&mode=${mode}`
+  )
+  //  ; .then(res => {
+  //   console.log(res.data)
+  //   res.data.carbonFootprint
+  // })
+  // .catch(err => console.log(err))
 }
 
 router.post('/trip', (req, res) => {
@@ -21,17 +23,13 @@ router.post('/trip', (req, res) => {
 })
 
 function getGoogleTrip(origin, destination, mode, transitMode) {
+  // return miles
   return axios.get(
-    `https://maps.googleapis.com/maps/api/directions/json?origin=${origin}&destination=${destination}&mode=${mode}&transit_mode=${transitMode}&key=${process.env.GOOGLE_KEY}`
+    `https://maps.googleapis.com/maps/api/directions/json?origin=${origin}&destination=${destination}&units=imperial&mode=${mode}&transit_mode=${transitMode}&key=${process.env.GOOGLE_KEY}`
   )
-  // .then(res => {
-  //   // console.log('Full REQ', res)
-  //   console.log('MODEUUUUU!!!!', mode, res.data.routes[0].legs[0].distance)
-  // })
-  // console.log('Working ! ', origin, destination, mode, transitMode)
 }
 
-router.post('/google-trip', (req, res, next) => {
+function handleGoogleResponse(req, res) {
   // Infos, needed, MODE, DISTANCE, DUREE
 
   let transitMode = null
@@ -39,33 +37,40 @@ router.post('/google-trip', (req, res, next) => {
   let driving = getGoogleTrip(origin, destination, 'driving', transitMode)
   let walking = getGoogleTrip(origin, destination, 'walking', transitMode)
   let training = getGoogleTrip(origin, destination, 'transit', 'train')
-  let busing = getGoogleTrip(origin, destination, 'transit', 'bus')
-  Promise.all([driving, walking, training, busing])
-    .then(res => {
-      // console.log('Promoise VALUES :', res)
-      // res.forEach()
-      let car = {
-        distance: res[0].data.routes[0].legs[0].distance,
-        time: res[0].data.routes[0].legs[0].duration,
+
+  return Promise.all([driving, walking, training])
+}
+
+router.post('/google-trip', (req, res, next) => {
+  handleGoogleResponse(req, res)
+    .then(response => {
+      let anyCar = {
+        distance: Math.floor(
+          response[2].data.routes[0].legs[0].distance.value / 1609.344
+        ),
+        time: response[0].data.routes[0].legs[0].duration,
       }
       let foot = {
-        distance: res[1].data.routes[0].legs[0].distance,
-        time: res[1].data.routes[0].legs[0].duration,
+        distance: response[1].data.routes[0].legs[0].distance.value,
+        time: response[1].data.routes[0].legs[0].duration,
       }
-      let train = {
-        distance: res[2].data.routes[0].legs[0].distance,
-        time: res[2].data.routes[0].legs[0].duration,
+      let transitRail = {
+        distance: Math.floor(
+          response[2].data.routes[0].legs[0].distance.value / 1609.344
+        ),
+        time: response[2].data.routes[0].legs[0].duration,
       }
-      let bus = {
-        distance: res[3].data.routes[0].legs[0].distance,
-        time: res[3].data.routes[0].legs[0].duration,
-      }
-      console.log('car :', car)
-      console.log('foot :', foot)
-      console.log('train :', train)
-      console.log('bus :', bus)
 
-      // res.send({ ca })
+      let railCarbon = getCarbonPrint(transitRail.distance, 'transitRail')
+      let carCarbon = getCarbonPrint(anyCar.distance, 'anyCar')
+      let footCarbon = 'not much'
+      console.log('RailCarbon', railCarbon)
+      Promise.all([railCarbon, carCarbon]).then(value => {
+        console.log(value)
+        let railCarbon = value[0].data.carbonFootprint
+        let carCarbon = value[1].data.carbonFootprint
+        res.send({ railCarbon, carCarbon, footCarbon })
+      })
     })
     .catch(err => console.log(err))
 })
