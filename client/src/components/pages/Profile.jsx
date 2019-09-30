7216759c1866d988163a08f24b62d996e9855f5f
Maxime <justine.gander@gmail.com>
2019-09-30 11:43:02 +0200

    css profile

--- client/src/components/pages/Profile.jsx
+++ client/src/components/pages/Profile.jsx
@@ -104,60 +104,86 @@ export default function Profile() {
 				<div className="profil_image">
 					<img style={{ height: '300px' }} src={user.picture} alt="img" />
 				</div>
-				<div>
-					<label>Name : </label>
-					<input type="text" name="name" value={user.name} onChange={handleOnChange} />
+				<div className="profile_detail_info">
+					<label>Name </label>
+					<input
+						className="input_profile"
+						type="text"
+						name="name"
+						value={user.name}
+						onChange={handleOnChange}
+					/>
 					<button onClick={handleClickButton} className="edit-btn">
 						Edit
 					</button>
 					<br />
-					<label>Email : </label>
-					<input type="text" name="email" value={user.email} onChange={handleOnChange} />
+					<label>Email </label>
+					<input
+						className="input_profile"
+						type="text"
+						name="email"
+						value={user.email}
+						onChange={handleOnChange}
+					/>
 					<button onClick={handleClickButton} className="edit-btn">
 						Edit
 					</button>
 					<br />
-					{/* <label>Password : </label>
-				<input type="password" name="password" value={user.password} onChange={handleOnChange} />
-				<button onClick={handleClickButton} className="edit-btn">
-					Edit
-				</button>
-				<br /> */}
-					<label>Profile picture : </label>
-					<input type="file" name="picture" onChange={handleOnChange} />
+					<label>Profile picture </label>
+					<input className="input_profile" type="file" name="picture" onChange={handleOnChange} />
 					<button onClick={handleClickButton} className="edit-btn">
 						Edit
 					</button>
 					<br />
 				</div>
-				<button>Delete account</button>
+				<button className="edit-btn">Delete account</button>
 			</div>
 			<div className="profile_trips">
 				<h1>Your trips</h1>
-				{trip.map((trips, i) => (
-					<ul key={i}>
-						<h2>Trip n° {[ i + 1 ]}</h2>
-						<li>Departure: {trips.departure}</li>
-						<li>Arrival: {trips.arrival}</li>
-						<li>Transport: {trips.transport}</li>
-						<li>Duration: {trips.duration}</li>
-						<li>Distance: {trips.distance} km</li>
-						<li>Cabron footprint: {trips.carbon} kg</li>
-					</ul>
-				))}
+				<div classname="trip_details">
+					{trip.map((trips, i) => (
+						<ul key={i}>
+							<h2>Trip n° {[ i + 1 ]}</h2>
+
+							<li>
+								<strong>Departure: </strong> {trips.departure}
+							</li>
+							<li>
+								<strong>Arrival: </strong>
+								{trips.arrival}
+							</li>
+							<li>
+								<strong>Transport: </strong>
+								{trips.transport}
+							</li>
+							<li>
+								<strong>Duration: </strong>
+								{trips.duration}
+							</li>
+							<li>
+								<strong>Distance: </strong>
+								{trips.distance} km
+							</li>
+							<li>
+								<strong>Cabron footprint: </strong>
+								{trips.carbon} kg
+							</li>
+						</ul>
+					))}
+				</div>
 			</div>
-			<div>
+			{/* <div>
 				<div>
 					<ErrorChart data={data} />
 				</div>
-			</div>
+			</div> */}
 		</div>
 	);
 }
-const ErrorChart = ({ data }) => {
-	return (
-		<div>
-			<Bar data={data} />
-		</div>
-	);
-};
+// const ErrorChart = ({ data }) => {
+// 	return (
+// 		<div>
+// 			<Bar data={data} />
+// 		</div>
+// 	);
+// };
