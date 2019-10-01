90b592f2d78d49e5e3e5b9f4c3472ab7c274286c
Loup A <loup.aparicio@gmail.com>
2019-10-01 20:39:51 +0200

    Polyline map

--- client/src/components/pages/MyMapComponent.jsx
+++ client/src/components/pages/MyMapComponent.jsx
@@ -1,93 +1,169 @@
-/*global google*/
-import React, { useEffect, useState } from 'react'
-import { withGoogleMap, GoogleMap, DirectionsRenderer } from 'react-google-maps'
+import React, { useState, useEffect } from 'react'
+import {
+  GoogleMap,
+  withScriptjs,
+  withGoogleMap,
+  Marker,
+  Polyline,
+  DirectionsRenderer,
+} from 'react-google-maps'
+import _ from 'lodash'
 
-export default function Map(props) {
-  console.log(props)
+const Map = React.memo(props => {
+  const mapMe = arr => {
+    return arr.map(a => a.lat_lngs[0])
+  }
 
-  const [directions, setDirections] = useState([])
-  // const [directions2, setDirections2] = useState([])
-
-  // const [userTravelMode, setUserTravelMode] = useState()
-
-  // function setNewDirectionsService(object) {
-  //   const directionsService = new window.google.maps.DirectionsService()
-  //   let travelModo = window.google.maps.TravelMode
-
-  //   directionsService.route(
-  //     {
-  //       origin: object.origin,
-  //       destination: object.destination,
-  // travelMode:
-  //   object.mode === 'DRIVING'
-  //     ? travelModo.DRIVING
-  //     : object.mode === 'TRANSIT'
-  //     ? travelModo.TRANSIT
-  //     : object.mode === 'WALKING'
-  //     ? travelModo.WALKING
-  //     : travelModo.BICYCLE,
-  //     },
-  //     (result, status) => {
-  //       if (status === window.google.maps.DirectionsStatus.OK) {
-  //         console.log(result)
-  //         setDirections(result)
-  //       } else {
-  //         console.error(`error fetching directions ${result}`)
-  //       }
-  //     }
-  //   )
-  // }
-
-  // useEffect(() => {
-  //   for (let i = 1; i <= props.trips.length; i++) {
-  //     setNewDirectionsService()
-  //   }
-  // }, [])
-
+  // const myWayPoints = props.markers.map(m => {
+  //   const str = m.position.lat + ',' + m.position.lng
+  //   return { location: str }
+  // })
+  // console.log(props.directions.routes[0].legs[0].steps)
+  console.log(props.directions)
   useEffect(() => {
-    const directionsService = new window.google.maps.DirectionsService()
-
-    let travelModo = window.google.maps.TravelMode
-
-    directionsService.route(
+    const DirectionsService = new window.google.maps.DirectionsService()
+    DirectionsService.route(
       {
-        origin: props.trip.origin,
-        destination: props.trip.destination,
-        travelMode:
-          props.trip.mode === 'DRIVING'
-            ? travelModo.DRIVING
-            : props.trip.mode === 'TRANSIT'
-            ? travelModo.TRANSIT
-            : props.trip.mode === 'WALKING'
-            ? travelModo.WALKING
-            : travelModo.BICYCLE,
+        origin: props.trips.origin,
+        destination: props.trips.destination,
+        travelMode: window.google.maps.TravelMode[props.trips.mode],
       },
       (result, status) => {
         if (status === window.google.maps.DirectionsStatus.OK) {
-          console.log(result)
-          setDirections(result)
+          props.handleDirections(result)
         } else {
-          console.error(`error fetching directions ${result}`)
+          console.error(`error fetching directions ${JSON.stringify(result)}`)
         }
       }
     )
+  }, [props.markers])
+
+  const polyOptions = {
+    strokeColor: 'red',
+    strokeOpacity: 0.5,
+    zIndex: 1,
+    strokeWeight: 5,
+  }
+  function handleMe(e) {
+    console.log(e, 'this is meee')
+  }
+  return (
+    <GoogleMap
+      onClick={props.handleMapClicked}
+      ref={props.onMapMounted}
+      defaultZoom={12}
+      center={props.center}
+      defaultCenter={{ lat: 48.866667, lng: 2.333333 }}
+    >
+      {/* {props.markers.map((marker, i) => (
+        <Marker
+          onClick={props.handleMarkerClick}
+          key={i}
+          position={marker.position}
+        />
+      ))} */}
+      {props.directions && (
+        <Polyline
+          onClick={handleMe}
+          options={polyOptions}
+          path={props.directions.routes[0].overview_path}
+        />
+      )}
+      <DirectionsRenderer directions={props.directions} />
+    </GoogleMap>
+  )
+})
+
+const WrapperMap = withScriptjs(withGoogleMap(Map))
+
+const AppMap = props => {
+  const [state, setState] = useState({})
+  const refs = {}
+
+  useEffect(() => {
+    setState({
+      ...state,
+      bounds: null,
+      center: {
+        lat: 48.866667,
+        lng: 2.333333,
+      },
+      markers: [],
+    })
+    /* eslint-disable */
   }, [])
 
-  const GoogleMapExample = withGoogleMap(props => (
-    <GoogleMap
-      defaultCenter={{ lat: 48.864716, lng: 2.349014 }}
-      defaultZoom={13}
-    >
-      {<DirectionsRenderer directions={directions} />}
-    </GoogleMap>
-  ))
+  const onMapMounted = ref => (refs.map = ref)
+  const onBoundsChanged = () => {
+    setState({
+      ...state,
+      bounds: refs.map.getBounds(),
+      center: refs.map.getCenter(),
+    })
+  }
 
+  const onPlacesChanged = () => {
+    const places = refs.searchBox.getPlaces()
+    const bounds = new window.google.maps.LatLngBounds()
+    places.forEach(place => {
+      if (place.geometry.viewport) {
+        bounds.union(place.geometry.viewport)
+      } else {
+        bounds.extend(place.geometry.location)
+      }
+    })
+    const nextMarkers = places.map(place => ({
+      position: {
+        lat: place.geometry.location.lat(),
+        lng: place.geometry.location.lng(),
+      },
+    }))
+
+    const nextCenter = _.get(nextMarkers, '0.position', state.center)
+    setState({
+      ...state,
+      center: nextCenter,
+      markers: [...state.markers, ...nextMarkers],
+    })
+  }
+
+  const handleDirections = result => {
+    setState({ ...state, directions: result })
+  }
+  const handleMarkerClick = props => {
+    console.log(props)
+    console.log(props.latLng.lat())
+    console.log(props.latLng.lng())
+  }
+  const handleMapClicked = e => {
+    setState({
+      ...state,
+      markers: [
+        ...state.markers,
+        { position: { lat: e.latLng.lat(), lng: e.latLng.lng() } },
+      ],
+    })
+  }
   return (
-    <div>
-      <GoogleMapExample
-        containerElement={<div style={{ height: `80vh`, width: '80vw' }} />}
-        mapElement={<div style={{ height: `100%` }} />}
+    <div style={{ height: '50vh' }}>
+      <WrapperMap
+        trips={props.trips}
+        handleMapClicked={handleMapClicked}
+        handleMarkerClick={handleMarkerClick}
+        directions={state.directions}
+        onBoundsChanged={onBoundsChanged}
+        center={state.center}
+        onMapMounted={onMapMounted}
+        markers={state.markers}
+        onPlacesChanged={onPlacesChanged}
+        handleDirections={handleDirections}
+        googleMapURL={`https://maps.googleapis.com/maps/api/js?key=AIzaSyC4R6AN7SmujjPUIGKdyao2Kqitzr1kiRg&v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_GOOGLE_APIKEY}`}
+        loadingElement={<div style={{ height: '100%' }} />}
+        containerElement={<div style={{ height: '100%' }} />}
+        mapElement={<div style={{ height: '100%' }} />}
       />
     </div>
   )
 }
+
+export default AppMap
--- client/src/components/pages/Test.jsx
+++ client/src/components/pages/Test.jsx
@@ -10,16 +10,11 @@ export default function Test() {
   let trip2 = { origin: 'Paris', destination: 'Brest', mode: 'TRANSIT' }
   let trip3 = { origin: 'Paris', destination: 'Brest', mode: 'DRIVING' }
   let trips = [trip1, trip2, trip3]
-  const MapLoader = withScriptjs(Map)
 
   return (
     <div>
       <button>TEST ME</button>
-      <MapLoader
-        trip={trip3}
-        googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_APIKEY}`}
-        loadingElement={<div style={{ height: `100%` }} />}
-      />
+      <Map trips={trip1} />
     </div>
   )
 }
