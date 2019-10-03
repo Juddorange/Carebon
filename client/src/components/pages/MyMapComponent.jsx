87cfc169e091ce8c2c806842d19153211f516575
Loup A <loup.aparicio@gmail.com>
2019-10-03 17:10:08 +0200

    remove useless google key in Mapcomponent

--- client/src/components/pages/MyMapComponent.jsx
+++ client/src/components/pages/MyMapComponent.jsx
@@ -140,7 +140,8 @@ const AppMap = props => {
         markers={state.markers}
         onPlacesChanged={onPlacesChanged}
         handleDirections={handleDirections}
-        googleMapURL={`https://maps.googleapis.com/maps/api/js?key=AIzaSyC4R6AN7SmujjPUIGKdyao2Kqitzr1kiRg&v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_GOOGLE_APIKEY}`}
+        // googleMapURL={`https://maps.googleapis.com/maps/api/js?key=AIzaSyC4R6AN7SmujjPUIGKdyao2Kqitzr1kiRg&v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_GOOGLE_APIKEY}`}
+        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_GOOGLE_APIKEY}`}
         loadingElement={<div style={{ height: '100%' }} />}
         containerElement={<div style={{ height: '100%' }} />}
         mapElement={<div style={{ height: '100%' }} />}
